5f5ebea5a86a6c8d73179de85a0863b8564a07fd
FelipeGCx <ggfelipegc@gmail.com>
2022-09-19 10:36:23 -0500

    addded main title component

---
+++ src/components/TheMainTitle.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <span></span>
+    <h1>{{ title }}</h1>
+    <span></span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+div {
+  display: flex;
+  gap: 2.5rem;
+  align-items: center;
+  padding: 2rem;
+  span {
+    width: 100%;
+    height: 0.2rem;
+    background-color: $secondary-color;
+  }
+  h1 {
+    text-transform: uppercase;
+    white-space: nowrap;
+    // margin: 0 1rem;
+  }
+}
+</style>
--- src/views/AllView.vue
+++ src/views/AllView.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <h1 class="main-title">All Content</h1>
+    <the-main-title :title="'All Content'" />
     <div v-for="i in 20" :key="i">
       {{ i }}
     </div>
@@ -8,8 +8,10 @@
 </template>
 
 <script>
+import TheMainTitle from "@/components/TheMainTitle.vue";
 export default {
   name: "AllView",
+  components: { TheMainTitle },
 };
 </script>
 
