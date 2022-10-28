f1d3b013ecd52147a62228f88ac549a50900ecea
FelipeGCx <ggfelipegc@gmail.com>
2022-10-27 19:56:27 -0500

    try implement verify token

--- src/mixins/mutations/vtoken.js
+++ src/mixins/mutations/vtoken.js
@@ -4,28 +4,21 @@ export default {
   methods: {
     async verifyToken(token) {
       console.log("token", token);
-      await this.$apollo
-        .mutate({
-          mutation: gql`
-            mutation VerifyToken($token: String) {
-              verifyToken(token: $token) {
-                data {
-                  id
-                }
-                error
+      return await this.$apollo.mutate({
+        mutation: gql`
+          mutation VerifyToken($token: String) {
+            verifyToken(token: $token) {
+              data {
+                id
               }
+              error
             }
-          `,
-          variables: {
-            token: token,
-          },
-        })
-        .then((result) => {
-          console.log(result);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+          }
+        `,
+        variables: {
+          token: token,
+        },
+      });
     },
   },
 };
