d50b09b1e098bccd4a8d92778d0024e000080369
felipegcx <ggfelipegc@gmail.com>
2024-10-20 17:26:36 -0500

    feat: update apigateway url

--- apollo.config.js
+++ apollo.config.js
@@ -3,7 +3,7 @@ module.exports = {
     service: {
       name: "my-app",
       // URL to the GraphQL API
-      url: "https://films-db-apigateway.vercel.app/",
+      url: "https://filmsdb-apigateway-production.up.railway.app",
     },
     // Files processed by the extension
     includes: ["src/**/*.vue", "src/**/*.js"],
