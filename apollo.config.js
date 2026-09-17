28554a83a0d7c9b95697ba17e7fb0c6de117bfdf
felipegcx <ggfelipegc@gmail.com>
2026-09-17 15:21:54 -0500

    fix: adjust name

--- apollo.config.js
+++ apollo.config.js
@@ -4,7 +4,7 @@ module.exports = {
     service: {
       name: "my-app",
       // URL to the GraphQL API
-      url: process.env.PUBLIC_API_GATEWAY_KEY,
+      url: process.env.PUBLIC_API_GATEWAY_URL,
     },
     // Files processed by the extension
     includes: ["src/**/*.vue", "src/**/*.js"],
